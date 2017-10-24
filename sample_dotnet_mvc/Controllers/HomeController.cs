1c58c78ea08744cf8addf9e3d6e22be1e597d96c
Sonu0985 <30734758+Sonu0985@users.noreply.github.com>
2017-10-24 12:57:03 -0400

    New Files for first project

---
+++ sample_dotnet_mvc/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sample_dotnet_mvc.Models;
+
+namespace sample_dotnet_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            ViewData["Title"]="Sai Message";
+            ViewData["Message"] = "Site Under Development";
+            return View();
+        }
+
+        public IActionResult Photo()
+        {
+            ViewData["Title"] = "Photo Gallery";
+            ViewData["Message"] = "Site Under Development";
+            return View();
+        }
+
+        public IActionResult Donation()
+        {
+            ViewData["Title"] = "Donation";
+            ViewData["Message"] = "Site Under Development";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About-New";
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sample_dotnet_mvc/Controllers/SideController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sample_dotnet_mvc.Models;
+
+namespace sample_dotnet_mvc.Controllers
+{
+    public class SideController : Controller
+    {
+
+		public IActionResult Saiblog()
+		{
+			ViewData["Title"] = "SaiBlog";
+			ViewData["Message"] = "Site Under Development";
+			return View();
+		}
+
+
+		public IActionResult Information()
+		{
+			ViewData["Title"] = "Information";
+			ViewData["Message"] = "Site Under Development";
+			return View();
+		}
+
+        public IActionResult Current()
+        {
+            ViewData["Title"] = "Current Project";
+            ViewData["Message"] = "Site Under Development";
+            return View();
+        }
+
+		public IActionResult FuturePlan()
+		{
+			ViewData["Title"] = "Future Plan Project";
+			ViewData["Message"] = "Site Under Development";
+			return View();
+		}
+
+		public IActionResult Suggestion()
+		{
+			ViewData["Title"] = "Suggestion";
+			ViewData["Message"] = "Site Under Development";
+			return View();
+		}
+
+		public IActionResult News()
+		{
+			ViewData["Title"] = "Future Plan Project";
+			ViewData["Message"] = "Site Under Development";
+			return View();
+		}
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page is from Side Controller.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
